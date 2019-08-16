526763bb7afeecdf7967de34486b6b72e24f970e
Richard van der Dys <richard.vanderdys@gmail.com>
2019-08-16 16:45:11 +0300

    Testing

---
+++ pages/test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from '../components/head';
+
+const Test = () => {
+  return (
+    <div>
+      <Head title="Test" />
+
+      <div className="hero">
+        <h1 className="title">Test this!</h1>
+        <p className="description">Yep yep yep</p>
+      </div>
+
+      <style jsx>{``}</style>
+    </div>
+  );
+};
+
+export default Test;
